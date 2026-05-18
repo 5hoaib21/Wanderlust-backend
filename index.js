ecdc91921adf56a141d29c95b7a49d722fffc405
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-18 08:26:30 +0600

    Deploying backend on Vercel

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ const client = new MongoClient(uri, {
   },
 });
 
-const JWKS = createRemoteJWKSet(
-  new URL("http://localhost:3000/api/auth/jwks")
-)
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req?.headers?.authorization;
@@ -31,23 +29,21 @@ const verifyToken = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
   const token = authHeader.split(" ")[1];
-  if(!token){
+  if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-try {
-   const {payload} = await jwtVerify(token, JWKS)
-  console.log(payload);
-  next();
-} catch (error) {
-  return res.status(403).json({message: "Forbidden"})
-  
-}
- 
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
 };
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("wanderlust");
     const destinationCollection = db.collection("destinations");
@@ -97,7 +93,7 @@ async function run() {
 
     // booking apis
 
-    app.get("/booking/:userId",verifyToken, async (req, res) => {
+    app.get("/booking/:userId", verifyToken, async (req, res) => {
       const { userId } = req.params;
       const result = await bookingsCollection
         .find({ userId: userId })
@@ -111,7 +107,7 @@ async function run() {
       res.json(result);
     });
 
-    app.delete("/booking/:bookingId",verifyToken, async (req, res) => {
+    app.delete("/booking/:bookingId", verifyToken, async (req, res) => {
       const { bookingId } = req.params;
       const result = await bookingsCollection.deleteOne({
         _id: new ObjectId(bookingId),
@@ -119,7 +115,7 @@ async function run() {
       res.json(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
